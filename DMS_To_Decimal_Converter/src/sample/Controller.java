92347bc2f4158a8fae9bc772e414057d8c5ddd97
Sapientior <sapientior_aet@gmail.com>
2021-03-21 22:32:20 -0700

    made it actually work now.

--- DMS_To_Decimal_Converter/src/sample/Controller.java
+++ DMS_To_Decimal_Converter/src/sample/Controller.java
@@ -1,48 +1,58 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Controller{
     //input fields
     @FXML
-    private double degreesInput;
+    private TextField degreesInput;
     @FXML
-    private double minutesInput;
+    private TextField minutesInput;
     @FXML
-    private double secondsInput;
+    private TextField secondsInput;
     @FXML
-    private double decimalInput;
+    private TextField decimalInput;
 
     //output fields
     @FXML
-    private double degreesOutput;
+    private TextField degreesOutput;
     @FXML
-    private double minutesOutput;
+    private TextField minutesOutput;
     @FXML
-    private double secondsOutput;
+    private TextField secondsOutput;
     @FXML
-    private double decimalOutput;
+    private TextField decimalOutput;
 
     public void DMSToDecimal() {
-        this.secondsInput /= 3600;
-        this.minutesInput /= 60;
-        this.degreesInput = this.degreesInput + this.minutesInput + this.secondsInput;
-        double number = (Math.round(this.degreesInput * 1000));
+        Double dInput = Double.parseDouble(this.degreesInput.getText());
+        Double mInput = Double.parseDouble(this.minutesInput.getText());
+        Double sInput = Double.parseDouble(this.secondsInput.getText());
+
+        sInput /= 3600;
+        mInput /= 60;
+        dInput = dInput + mInput + sInput;
+        double number = (Math.round(dInput * 1000));
         number /= 1000;
+        Double newNum = number;
 
         // do whatever output here
-        System.out.println("Decimal Output: " + number);
+        this.decimalOutput.setText(newNum.toString());
     }
 
     public void decimalToDMS() {
-        this.degreesOutput = Math.floor(this.decimalInput); // rounds down the double
-        double minutesAndSeconds = (this.decimalInput % 1) * 60; // multiplies the decimal by 60 to give us minutes & seconds.
-        this.minutesOutput = Math.floor(minutesAndSeconds); // rounds down the previous decimal to give us just the minutes.
-        this.secondsOutput = Math.round((minutesAndSeconds % 1) * 60);
+        Double decimalInput = Double.parseDouble(this.decimalInput.getText());
+
+        Double degrees =  Math.floor(decimalInput); // rounds down the double
+        Double minutesAndSeconds = (decimalInput % 1) * 60; // multiplies the decimal by 60 to give us minutes & seconds.
+        Double minutes = Math.floor(minutesAndSeconds); // rounds down the previous decimal to give us just the minutes.
+        double seconds = Math.round((minutesAndSeconds % 1) * 60);
+        Double newSecs = seconds;
 
         // do whatever output here
-//        System.out.println("Degrees: " + degrees);
-//        System.out.println("Minutes: " + minutes);
-//        System.out.println("Seconds: " + seconds);
+        this.degreesOutput.setText(degrees.toString());
+        this.minutesOutput.setText(minutes.toString());
+        this.secondsOutput.setText(newSecs.toString());
+
     }
 }
--- DMS_To_Decimal_Converter/src/sample/Main.java
+++ DMS_To_Decimal_Converter/src/sample/Main.java
@@ -19,30 +19,5 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-//        decimalToDMS(10.413);
-//        DMSToDecimal(10, 24, 47);
     }
-//
-//    public static void decimalToDMS(double number) {
-//        double degrees = Math.floor(number); // rounds down the double
-//        double minutesAndSeconds = (number % 1) * 60; // multiplies the decimal by 60 to give us minutes & seconds.
-//        double minutes = Math.floor(minutesAndSeconds); // rounds down the previous decimal to give us just the minutes.
-//        double seconds = Math.round((minutesAndSeconds % 1) * 60);
-//
-//        // do whatever output here
-//        System.out.println("Degrees: " + degrees);
-//        System.out.println("Minutes: " + minutes);
-//        System.out.println("Seconds: " + seconds);
-//    }
-//
-//    public static void DMSToDecimal(double degrees, double minutes, double seconds) {
-//        seconds /= 3600;
-//        minutes /= 60;
-//        degrees = degrees + minutes + seconds;
-//        double number = (Math.round(degrees * 1000));
-//        number /= 1000;
-//
-//        // do whatever output here
-//        System.out.println("Decimal Output: " + number);
-//    }
 }
